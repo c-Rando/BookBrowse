249f21739a89d9eda469cb309a38d9c5fe36ba88
C. Rando <crando@gmail.com>
2021-07-14 18:52:03 -0400

    updated rouutes in server

--- 02-Homework/Develop/server/routes/api/user-routes.js
+++ 02-Homework/Develop/server/routes/api/user-routes.js
@@ -10,13 +10,17 @@ const {
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
+// /api/users/ POST PUT
 // put authMiddleware anywhere we need to send a token for verification of user
 router.route('/').post(createUser).put(authMiddleware, saveBook);
 
+// /api/users/login POST
 router.route('/login').post(login);
 
+// /api/users/me GET
 router.route('/me').get(authMiddleware, getSingleUser);
 
+// /api/users/books/:bookId DELETE
 router.route('/books/:bookId').delete(authMiddleware, deleteBook);
 
 module.exports = router;
